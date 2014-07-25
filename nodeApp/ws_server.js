3fe0393dc44bc4ffdad34b43008304208c3b67a3
vakha11 <vakha11@gmail.com>
2014-07-25 19:28:56 +0300

    request page with description, helpers, title and address

--- nodeApp/ws_server.js
+++ nodeApp/ws_server.js
@@ -1,22 +1,40 @@
-var http = require('http'),
-    io = require('socket.io'),
-	rpc = require('jsonrpc-ws')
+var http = require('http');
+var io = require('socket.io');
+var rpc = require('jsonrpc-ws');
+
+var mongojs = require('mongojs');
+var db = mongojs("127.0.0.1/pomogalka",["requests","users"]);
 
 var RpcFunctions = {
-	add: function (a, b) {
-		var r = a + b;
-		return r;
-	},
+    add: function (a, b) {
+        resp = this;
+        resp.send(a+b);
+
+        //return { _id: "53c151cceed92e10d09530ab", name: '221weqdeq', value: 12312312 };
+    },
+    getAllMarkers: function () {
+        resp = this;
+        db.requests.find(
+            {},
+            {coord:1, _id:0},
+            function (err, data){
+                var res = data.map( function(elem){ return elem.coord } );
+                console.log(res);
+                resp.send(res);
+            }
+        );
+        //return { _id: "53c151cceed92e10d09530ab", name: '221weqdeq', value: 12312312 };
+    }
 };
 
 rpc.exposeModule('rpc', RpcFunctions);
 
 server = http.createServer(function(req, res){
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('<h1>Hello world</h1>');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello world</h1>');
 });
 
 var socket = io.listen(server);
 rpc.listen(socket);
 
-server.listen(8080);
+server.listen(8489);
